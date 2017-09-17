3791fbefba7eac9b99c5e7fb24f51805da994de5
Rafa Jiménez Seibane <rjseibane@gmail.com>
2017-09-17 20:55:07 +0200

    Add users functionality + tests

--- model/entity/user.go
+++ model/entity/user.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	gorm.Model
-	Alias    string `json:"match" groups:"readable,visible"`
+	Alias    string `json:"alias" groups:"readable,visible"`
 	Password string `json:"-" groups:"readable"`
 	Email    string `json:"match" groups:"readable"`
 }
