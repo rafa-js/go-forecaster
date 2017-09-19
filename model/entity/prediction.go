47340f5ff327899aacdb4ec0f38eadb86ff9ef6c
Rafa Jiménez Seibane <rjseibane@gmail.com>
2017-09-19 03:38:09 +0200

    Add hidden prediction service

--- model/entity/prediction.go
+++ model/entity/prediction.go
@@ -6,8 +6,8 @@ import (
 
 type Prediction struct {
 	gorm.Model
-	Match         Match  `json:"match" groups:"readable,visible" gorm:"index:idx_match save_associations:false"`
-	FromUser      User   `json:"fromUser" groups:"readable,visible" gorm:"index:idx_match save_associations:false"`
+	Match         Match  `json:"match" groups:"readable,visible" gorm:"index:idx_match"`
+	FromUser      User   `json:"fromUser" groups:"readable,visible" gorm:"index:idx_match "`
 	HomeTeamGoals int    `json:"homeTeamGoals" groups:"readable,visible"`
 	AwayTeamGoals int    `json:"awayTeamGoals" groups:"readable,visible"`
 }
