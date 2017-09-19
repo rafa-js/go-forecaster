63c9e1166d23fd242e7987e57f5a72d37205ad7d
Rafa Jiménez Seibane <rjseibane@gmail.com>
2017-09-19 03:08:32 +0200

    Add hidden prediction service

--- model/manager/match_manager.go
+++ model/manager/match_manager.go
@@ -14,7 +14,7 @@ func (manager MatchManager) GetCalendar() []entity.Match {
 	return matches
 }
 
-func (manager MatchManager) AddOrUpdateMatch(match *entity.Match) bool{
+func (manager MatchManager) AddOrUpdateMatch(match *entity.Match) bool {
 	currentMatch := entity.Match{}
 	manager.DB.Where(entity.Match{Competition: match.Competition, MatchDay: match.MatchDay,
 		HomeTeamName: match.HomeTeamName, AwayTeamName: match.AwayTeamName}).First(&currentMatch)
@@ -23,6 +23,7 @@ func (manager MatchManager) AddOrUpdateMatch(match *entity.Match) bool{
 		match.ID = currentMatch.ID
 	}
 	manager.DB.Save(match)
+	return isNewMatch
 }
 
 func CreateMatchManager() MatchManager {
