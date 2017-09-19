8490d62bce3bebb87377e6e344edd554a9b39f28
Rafa Jiménez Seibane <rjseibane@gmail.com>
2017-09-19 03:51:47 +0200

    Add hidden prediction service

--- model/entity/hidden_prediction.go
+++ model/entity/hidden_prediction.go
@@ -6,9 +6,9 @@ import (
 
 type HiddenPrediction struct {
 	gorm.Model
-	FromUser           User   `json:"fromUser" groups:"readable,visible" gorm:"index:idx_hidden_prediction"`
-	FromUserID         uint
-	Match              Match  `json:"match" groups:"readable,visible" gorm:"index:idx_hidden_prediction"`
-	MatchID            uint
+	FromUser           User   `json:"fromUser" groups:"readable,visible"`
+	FromUserID         uint   `gorm:"index:idx_hidden_prediction"`
+	Match              Match  `json:"match" groups:"readable,visible"`
+	MatchID            uint   `gorm:"index:idx_hidden_prediction"`
 	CypheredPrediction string `json:"cypheredPrediction" groups:"readable,visible"`
 }
--- model/entity/prediction.go
+++ model/entity/prediction.go
@@ -6,10 +6,10 @@ import (
 
 type Prediction struct {
 	gorm.Model
-	Match         Match  `json:"match" groups:"readable,visible" gorm:"index:idx_match"`
-	MatchID            uint
+	Match         Match  `json:"match" groups:"readable,visible"`
+	MatchID       uint   `gorm:"index:idx_match"`
 	FromUser      User   `json:"fromUser" groups:"readable,visible" gorm:"index:idx_match "`
-	FromUserID         uint
+	FromUserID    uint   `gorm:"index:idx_match"`
 	HomeTeamGoals int    `json:"homeTeamGoals" groups:"readable,visible"`
 	AwayTeamGoals int    `json:"awayTeamGoals" groups:"readable,visible"`
 }
