4120d3dfcdcfcdc45c29120b9baf138b685ee75b
Rafa Jiménez Seibane <rjseibane@gmail.com>
2017-09-19 04:07:27 +0200

    Add hidden prediction service

--- model/manager/hidden_prediction_manager.go
+++ model/manager/hidden_prediction_manager.go
@@ -10,17 +10,11 @@ type HiddenPredictionManager struct {
 
 func (manager HiddenPredictionManager) AddPrediction(hiddenPrediction *entity.HiddenPrediction) bool {
 	err := manager.DB.Create(hiddenPrediction).Error
-	if err != nil{
-		panic(err)
-	}
 	return err != nil
 }
 
 func (manager HiddenPredictionManager) UpdatePrediction(hiddenPrediction *entity.HiddenPrediction) bool {
 	err := manager.DB.Model(&hiddenPrediction).Update("CypheredPrediction").Error
-	if err != nil{
-		panic(err)
-	}
 	return err != nil
 }
 
