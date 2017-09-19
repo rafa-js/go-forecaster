c5534ab06a1d3718dd14b7893520fba2366a93ab
Rafa Jiménez Seibane <rjseibane@gmail.com>
2017-09-19 04:15:56 +0200

    Add hidden prediction service

--- model/manager/user_manager.go
+++ model/manager/user_manager.go
@@ -2,6 +2,8 @@ package manager
 
 import (
 	"github.com/server-forecaster/model/entity"
+	"github.com/server-forecaster/utils"
+	"time"
 )
 
 type UserManager struct {
@@ -9,6 +11,7 @@ type UserManager struct {
 }
 
 func (manager UserManager) AddUser(user *entity.User) bool {
+	user.AuthToken = utils.MD5(time.Now().String())
 	manager.DB.Create(&user)
 	return !manager.DB.NewRecord(user)
 }
@@ -16,7 +19,7 @@ func (manager UserManager) AddUser(user *entity.User) bool {
 func (manager UserManager) GetUserByAlias(alias string) *entity.User {
 	user := entity.User{}
 	manager.DB.Where("Alias = ?", alias).First(&user)
-	if user.Alias == ""{
+	if user.Alias == "" {
 		return nil
 	} else {
 		return &user
@@ -26,13 +29,12 @@ func (manager UserManager) GetUserByAlias(alias string) *entity.User {
 func (manager UserManager) GetUserByToken(token string) *entity.User {
 	user := entity.User{}
 	manager.DB.Where("auth_token = ?", token).First(&user)
-	if user.Alias == ""{
+	if user.Alias == "" {
 		return nil
 	} else {
 		return &user
 	}
 }
-
 
 func CreateUserManager() UserManager {
 	return UserManager{BaseManager: Create()}
