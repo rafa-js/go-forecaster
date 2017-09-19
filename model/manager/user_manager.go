82cb67a001e9d96fa634a39350bd1cb38cf68c16
Rafa Jiménez Seibane <rjseibane@gmail.com>
2017-09-19 03:11:53 +0200

    Add hidden prediction service

--- model/manager/user_manager.go
+++ model/manager/user_manager.go
@@ -25,7 +25,7 @@ func (manager UserManager) GetUserByAlias(alias string) *entity.User {
 
 func (manager UserManager) GetUserByToken(token string) *entity.User {
 	user := entity.User{}
-	manager.DB.Where("AuthToken = ?", token).First(&user)
+	manager.DB.Where("auth_token = ?", token).First(&user)
 	if user.Alias == ""{
 		return nil
 	} else {
