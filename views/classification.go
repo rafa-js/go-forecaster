c2577a777ce252f0c84ad57497140b51bdd1f397
Rafa Jiménez Seibane <rjseibane@gmail.com>
2017-09-24 18:20:42 +0200

    Reveal hidden predictions

--- views/classification.go
+++ views/classification.go
@@ -13,7 +13,7 @@ func GetClassification(writer http.ResponseWriter, request *http.Request) {
 	o := &sheriff.Options{
 		Groups: []string{"visible"},
 	}
-	data, err := sheriff.Marshal(o, classification.Scores[1].Hits[0].Match)
+	data, err := sheriff.Marshal(o, classification)
 	if err != nil {
 		writer.WriteHeader(http.StatusInternalServerError)
 	} else {
